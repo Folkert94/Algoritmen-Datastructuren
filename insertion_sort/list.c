ecd417289682ff904ed37b998bb35b30cc9ce726
Folkert Stijnman <folkertstijnman@hotmail.com>
2019-02-19 11:17:28 +0100

    v1 insort

---
+++ insertion_sort/list.c
@@ -0,0 +1,177 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "list.h"
+
+/*
+ * TODO: A lot of code missing here. You will need to add implementations for
+ * all the functions described in list.h here.
+ *
+ * Start by adding the definitions for the list and node structs. You may
+ * implement any of the Linked List versions discussed in the lecture, which
+ * have some different trade-offs for the functions you will need to write.
+ *
+ * Note: The function prototypes in list.h assume the most basic Singly Linked
+ * List. If you build some other version, you may not need all of the function
+ * arguments for all of the described functions. This will produce a warning,
+ * which you can suppress by adding a simple if-statement to check the value
+ * of the unused parameter.
+ *
+ * Also, do not forget to add any required includes at the top of your file.
+ */
+
+struct list {
+    struct node *head;
+};
+
+struct node {
+    int num;
+    struct node *next;
+};
+
+struct list* list_init(void) {
+    struct list *l = malloc(sizeof(struct list));
+    l->head = NULL;
+    return l;
+}
+
+struct node* list_new_node(int num) {
+    struct node *n = malloc(sizeof(struct node));
+    n->num = num;
+    n->next = NULL;
+    return n;
+}
+
+struct node* list_head(struct list* l) {
+    if (l->head == NULL) {
+        return NULL;
+    }
+    return l->head;
+}
+
+struct node* list_next(struct node* n) {
+    if (n == NULL) {
+        return NULL;
+    }
+    return n->next;
+}
+
+int list_add_front(struct list* l, struct node* n) {
+    if (l->head == NULL) {
+    l->head = n;
+    return 0;
+    }
+    n->next = l->head;
+    l->head = n;
+    return 0;
+}
+
+struct node* list_tail(struct list* l) {
+    if (l->head == NULL) {
+        return NULL;
+    }
+    struct node* tmp;
+    struct node* tmp1;
+
+    tmp = l->head;
+    while (tmp != NULL) {
+        tmp1 = tmp;
+        tmp = tmp->next;
+    }
+    return tmp1;
+}
+
+struct node* list_prev(struct list* l, struct node* n) {
+    struct node* current = l->head;
+    if (current == n) {
+        return NULL;
+    }
+    while (1) {
+        if (current->next == n) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+int list_add_back(struct list* l, struct node* n) {
+    if (l->head == NULL) {
+        l->head = n;
+        return 0;
+    }
+    struct node *current = l->head;
+    while (1) {
+        if (current->next == NULL)
+        {
+            current->next = n;
+            break;
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
+int list_node_value(struct node* n) {
+    if (n == NULL) {
+        return 0;
+    }
+    return n->num;
+}
+
+int list_unlink_node(struct list* l, struct node* n) {
+    if (l->head == n) {
+        l->head = n->next;
+        return 0;
+    }
+
+    struct node* temp = l->head;
+    struct node* prev;
+    while (temp != n) {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = n->next;
+    return 0;
+
+    }
+
+
+void list_free_node(struct node* n) {
+    free(n);
+}
+
+int list_cleanup(struct list* l) {
+    struct node* tmp;
+    while (l->head != NULL) {
+        tmp = l->head;
+        l->head = tmp->next;
+        free(tmp);
+    }
+    free(l);
+    return 0;
+}
+
+int list_node_present(struct list* l, struct node* n) {
+    ;
+}
+
+int list_insert_after(struct list* l, struct node* n, struct node* m) {
+    ;
+}
+
+int list_insert_before(struct list* l, struct node* n, struct node* m) {
+    ;
+}
+
+int list_length(struct list* l) {
+    ;
+}
+
+struct node* list_get_ith(struct list* l, int i) {
+    ;
+}
+
+struct list* list_cut_after(struct list* l, struct node* n) {
+    ;
+}
